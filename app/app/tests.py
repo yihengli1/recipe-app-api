1ebd459f07ac8c63753a686caa2a4332146fcbb9
YiggidyYi <105264876+YiggidyYi@users.noreply.github.com>
2024-06-27 10:44:38 -0700

    postgres

---
+++ app/app/tests.py
@@ -0,0 +1,22 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+
+from . import calc
+
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
+
+    def test_add_numbers(self):
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        res = calc.subtract(15, 10)
+
+        self.assertEqual(5, res)
