ee8fef7c4f04a9acaa60b767ea27cc5353b7e6c4
YiggidyYi <105264876+YiggidyYi@users.noreply.github.com>
2024-06-29 11:34:46 -0700

    patches

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,11 @@
+"""
+Django command to wait for db
+"""
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django command to wait for db"""
+
+    def handle(self, *args, **options):
+        pass
