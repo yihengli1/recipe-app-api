fd1df6d3d7f4e65bb12b1d1ca2dfd99bb27884b5
YiggidyYi <105264876+YiggidyYi@users.noreply.github.com>
2024-07-07 16:25:40 -0700

    me endpoint

--- app/user/views.py
+++ app/user/views.py
@@ -1,7 +1,23 @@
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializer
+from user.serializers import (UserSerializer, AuthTokenSerializer)
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManagerUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_objects(self):
+        return self.request.user
